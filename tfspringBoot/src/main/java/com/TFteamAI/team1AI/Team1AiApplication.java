62e4e6c3ef16a0fc60e50b7fd231bc96848ac482
312-11\cxas6673 <cxas6673@naver.com>
2025-02-07 10:01:11 +0900

    백 & 프론트와 파이썬 서버 통신 및 ai.html 영상 출력 완료

--- tfspringBoot/src/main/java/com/TFteamAI/team1AI/Team1AiApplication.java
+++ tfspringBoot/src/main/java/com/TFteamAI/team1AI/Team1AiApplication.java
@@ -2,6 +2,9 @@ package com.TFteamAI.team1AI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Team1AiApplication {
@@ -9,5 +12,5 @@ public class Team1AiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Team1AiApplication.class, args);
 	}
-
 }
+
