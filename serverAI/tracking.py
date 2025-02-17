2cb5035158a13957ce9b6aad4cd0cee1d28c9494
312-12\jskim <jscode77@gmail.com>
2025-02-17 09:30:05 +0900

    tracking 1차 완료

--- serverAI/tracking.py
+++ serverAI/tracking.py
@@ -1,6 +1,7 @@
 # pip install python-dotenv ->.env 파일에서 환경변수 값 불러오기 위함
 # pip install Pillow numpy requests ultralytics paho-mqtt opencv-python
 # pip install fastapi
+# pip install uvicorn pydantic requests -> uvicorn 만 설치됨(기존 이미 설치 완료
 # mosquitto 실행(cmd) : mosquitto -c mosquitto.conf -v
 # mosquitto 종료(cmd) : net stop mosquitto
 
@@ -14,8 +15,7 @@ import numpy as np
 from ultralytics import YOLO
 import paho.mqtt.client as mqtt  # 브로커 추가
 import cv2
-from saveData import save_data, SetData
-import datetime
+from saveData import save_data
 
 # 공통 경로/이름 정의(환경변수 값 사용)
 load_dotenv()
@@ -36,15 +36,9 @@ track_history = defaultdict(lambda :[]) #  Store the track history
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
 
-# 객체 감지용 색상 함수
-def get_colors(num_colors):
-    np.random.seed(3)
-    colors=[tuple(np.random.randint(0, 255, 3).tolist()) for _ in range(num_colors)]
-    return colors
 
 class_names = model.names   # 모델에서 받은 클래스 이름
 num_classes = len(class_names)  # 클래스 번호
-colors = get_colors(num_classes)    # 라벨링 박스 컬러색
 
 client.on_connect = on_connect
 
@@ -57,51 +51,44 @@ def cam_run():
     print(f"ipCam500 연결...")
     return cap
 
-def time_formater():
-    now = datetime.datetime.now()
-    time = now.strftime('%Y/%m/%d %H:%M:%S')
-    return time
+def tracking(frame:np.array):
+    results = model.track(frame, persist=True, conf=0.3, iou=0.5)  # 추적 활성화, 신뢰도 및 IoU 임계값 조정
+    # persist=True 인수는 트래커에게 현재 이미지 또는 프레임이 시퀀스의 다음 이미지이며 현재 이미지에서 이전 이미지의 트랙을 예상하도록 지시 ->프레임간 ID 유지
+    if results[0].boxes.id is None: # 탐지된 객체 없을 때
+        track_ids = []    # 빈리스트 반환
+    else:
+        track_ids = results[0].boxes.id.int().cpu().tolist()  # boxes id 정보를 int 형태 list로  cpu로 이동
+    boxes = results[0].boxes.xywh.int().cpu()  # ultralytics library
+    annotated_frame = results[0].plot().copy()  # 시각화 배열
+    class_ids = results[0].boxes.cls.int().cpu().tolist()
+    data = results[0].to_json()
+    save_data(data, MODEL_NAME.split(".")[0])
+    for box, track_id, class_id in zip(boxes, track_ids, class_ids):
+        x, y, w, h = box
+        track = track_history[track_id]  # 추적 기록
+        track.append((float(x), float(y)))  # x, y 포인트 추적기록에 추가
+        if len(track) > 30:  # retain 90 tracks for 90 frames
+            track.pop(0)
 
-def make_set(track, label):
-    now = time_formater()
-    tr = SetData(label, track, now)
-    data = tr.change_json()
-    save_data(data)
-
-
+            # # Draw the tracking lines
+        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+        cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+    cv2.imshow("tracking", annotated_frame)
+    return annotated_frame, data
 
 # 객체탐지 반복루프:main 실행시
 if __name__=='__main__':
     cap = cam_run()
     while cap.isOpened():
         success, frame = cap.read()
-        if success: # 있으면 추적
-            results = model.track(frame, persist=True)  # 추적 활성화
-            # persist=True 인수는 트래커에게 현재 이미지 또는 프레임이 시퀀스의 다음 이미지이며 현재 이미지에서 이전 이미지의 트랙을 예상하도록 지시
-            boxes = results[0].boxes.xywh.int().cpu()  # ultralytics library
-            annotated_frame = results[0].plot().copy()  # 시각화 배열
-            class_ids = results[0].boxes.cls.int().cpu().tolist()
-            track_ids = results[0].boxes.id.int().cpu().tolist()  # boxes id 정보를 int 형태 list로  cpu로 이동
-
-            for result in results:
-                data = result.to_json()
-                save_data(data)
-                for box, track_id, class_id in zip(boxes, track_ids, class_ids):
-                    x, y, w, h = box
-                    track = track_history[track_id]  # 추적 기록
-                    track.append((float(x), float(y)))  # x, y 포인트 추적기록에 추가
-                    if len(track) > 30:  # retain 90 tracks for 90 frames
-                        track.pop(0)
-
-                    # # Draw the tracking lines
-                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
-            cv2.imshow("tracking", annotated_frame)
-
-            _, buffer = cv2.imencode('.jpg',annotated_frame)
-            jpg_as_text = base64.b64encode(buffer).decode('utf-8')
-            payload=json.dumps({"image":jpg_as_text})
-            client.publish(TOPIC, payload)
+        if not success: # 있으면 추적
+            print("cam 연결 실패")
+            break
+        result_frame, data = tracking(frame)
+        _, buffer = cv2.imencode('.jpg',result_frame)
+        jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+        payload=json.dumps({"image":jpg_as_text, "result":data})
+        client.publish(TOPIC, payload)
         # 10ms 기다리고 다음 프레임으로 전환, Esc누르면 while 강제 종료
         if cv2.waitKey(10) == 27:
             break
